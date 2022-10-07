3f8d1c0d3980d54be399a1999126b7ce02e8cba4
b <b@bs-MacBook-Pro.local>
2022-10-06 21:54:00 -0400

    updated most of the code needed for this game to work

---
+++ app.js
@@ -0,0 +1,90 @@
+const scoreDisplay = document.getElementById('score-display')
+const questionDisplay = document.getElementById('question-display')
+
+const questions = [
+    {
+        quiz:['value', 'estimate', 'evaluate'],
+        option: ['jury','assess'],
+        correct: 2,
+    },
+    {
+        quiz:['close', 'near', 'next'],
+        option: ['trace','adjacent'],
+        correct: 2,
+    },
+    {
+        quiz:['foreign', 'national', 'ethnic'],
+        option: ['domestic','exotic'],
+        correct: 2,
+    },
+    {
+        quiz:['assume', 'insight', 'weather'],
+        option: ['forecast','sustainable'],
+        correct: 1,
+    },
+    {
+        quiz:['fast', 'quick', 'prompt'],
+        option: ['jury','assess'],
+        correct: 2,
+    },
+    {
+        quiz:['savior', 'courage', 'whitt'],
+        option: ['hero','villain'],
+        correct: 1,
+    }
+]
+
+let score = 0
+let clicked = []
+scoreDisplay.textContent = score
+
+function displayQuestions() {
+    questions.forEach(question => {
+      const questionBox =   document.createElement('div')
+      questionBox.classList.add('question-box')
+
+        const logoDisplay = document.createElement('h1')
+        logoDisplay.textContent = "✏️"
+        questionBox.append(logoDisplay)
+
+        question.quiz.forEach(tip => {
+          const tipText = document.createElement('p')
+          tipText.textContent = tip
+          questionBox.append(tipText)
+        })
+
+        const questionButtons = document.createElement('div')
+        questionButtons.classList.add('question-buttons')
+        questionBox.append(questionButtons)
+
+        question.option.forEach((option, optionIndex) => {
+            const questionButton = document.createElement('button')
+            questionButton.classList.add('question-button')
+            questionButton.textContent = option
+
+
+            questionButton.addEventListener('click', () => checkAnswer(questionButton, option, optionIndex +1, question.correct))
+
+            questionButtons.append(questionButton)
+        })
+        
+
+      questionDisplay.append(questionBox)
+    })
+}
+
+displayQuestions()
+
+function checkAnswer(questionButton, option, optionIndex, correctAnswer) {
+    console.log('option', option)
+    console.log('optionIndex', optionIndex)
+    if(optionIndex === correctAnswer) {
+        score++
+        scoreDisplay.textContent = score
+    } else {
+        score--
+        scoreDisplay.textContent = score
+    }
+    clicked.push(option)
+    questionButton.disabled = clicked.includes(option)
+}
